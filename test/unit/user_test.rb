38035a1deacc4888f562a0c3967f21bacc368920
Rod McLaughlin <rodmclaughlin@gmail.com>
2013-02-16 19:08:17 -0800

    I don't know why I thought the tests were passing. Turns out that create_user tries to use user_name instead of email.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,7 +5,19 @@ class UserTest < ActiveSupport::TestCase
   def test_should_create_user
     n = User.count
     user = create_user
+    raise user.errors.full_messages.join( ' ' ) unless n + 1 == User.count
+  end
+
+  def test_should_create_admin_user
+    n = User.count
+    user = create_user( { :email => User::ADMIN_EMAIL } )
     assert_equal n + 1, User.count
+    r = Role.count
+    u = user.roles.size
+    role = Role.new( { :rolename => User::ADMIN } )
+    assert_equal r + 1, Rold.count
+    user.roles << role
+    assert_equal u + 1, user.roles.size
   end
 
 end
