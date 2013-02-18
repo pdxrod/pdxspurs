53db6e4cfc67247f1abc897fd1c65ba51fba9d29
Rod McLaughlin <rodmclaughlin@gmail.com>
2013-02-17 16:35:11 -0800

    Now we're getting somewhere. Complaining about the lack of a UserSessionsController. That's easy to fix - I've got a few of those.

--- test/integration/basic_login_test.rb
+++ test/integration/basic_login_test.rb
@@ -5,7 +5,7 @@ class BasicLoginTest < ActionDispatch::IntegrationTest
   def test_should_get_home_page
     get '/'
     assert response.body.include?(          'Log in' )
-    assert false == response.body.include?( 'Log out' )
+    assert false == response.body.include?( 'Log out')
   end
   
   def test_should_log_in
@@ -16,12 +16,13 @@ class BasicLoginTest < ActionDispatch::IntegrationTest
     assert users.size == 1
     @user = users[ 0 ]
     assert @user.email == JUANDE
-    
-    get '/'
+   
+    visit '/' 
+    click_link "Log in"
     fill_in 'user_session_email', :with => @user.email
     fill_in 'user_session_password', :with => User::VALID_PASSWORD
-    click_link "Log in"
-    assert response.body.include?(          'Log out' )
+    click_button LOGIN_BUTTON      
+    assert response.body.include?(          'Log out')
     assert false == response.body.include?( 'Log in' )
 
   end
