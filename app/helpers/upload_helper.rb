aaaa7e633a5fd26ee02cd043b72daeca514c8ddb
z <x@y.com>
2017-04-20 13:10:00 +0545

    Changed pdxspurs.com to be a simple demo of the new git man page generator - just one file - index.html

--- app/helpers/upload_helper.rb
+++
@@ -1,5 +0,0 @@
-module UploadHelper
-  UPLOAD_TEMPLATE = 'app/views/upload/uploadfile.html.erb'
-end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,5 @@
 Pdxspurs::Application.routes.draw do
 
-  resources :lists, :posts, :users, :user_sessions, :password_resets
-
-  root :to =>           'main#index'
-  
-  get    'login' =>     'user_sessions#new',     :as => :login
-  get    'logoff' =>    'user_sessions#logoff',  :as => :logoff
-  delete 'logout' =>    'user_sessions#destroy', :as => :logout
-
-  post   'upload/uploadfile' => "upload#uploadfile"  
-  get    'upload'            => "upload#index"  
-
-  post 'password_resets/new'    => "password_resets#new"
-  put  'password_resets/new'    => "password_resets#new"
-  put  'password_resets/update' => "password_resets#update"
-
 end
 
 
